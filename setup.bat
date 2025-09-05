401a616873ef03d1b6dc0b170a03a62232b43969
Toan7042 <toan.hoangjoy2@gmail.com>
2025-09-05 09:22:18 +0700

    tf

--- setup.bat
+++ setup.bat
@@ -1,8 +1,4 @@
 @echo off
-tasklist /FI "IMAGENAME eq Docker Desktop.exe" 2>NUL | find /I /N "Docker Desktop.exe">NUL || (
-    start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-    timeout /t 15 >nul
-)
 set /p "APIKEY=Vui long nhap API key cua ban: "
 if not exist "moviegeek_config.zip" (echo LOI: Khong tim thay file moviegeek_config.zip. & pause & exit /b)
 powershell -Command "Expand-Archive -Path 'moviegeek_config.zip' -DestinationPath './' -Force"
